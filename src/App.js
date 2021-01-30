d1c74d2c1c500433dc953dda7fc86d8112b1a8c9
jdeleonardis <jdeleonardis@hotmail.com>
2021-01-30 11:49:58 -0500

    initial

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import FadeIn from "react-fade-in";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      <FadeIn>
+      <ul>
+        <li>item 1</li>
+        <li>item 2</li>
+        <li>item 3</li>
+        <li>item 4</li>
+        <li>item 5</li>
+        <li>item 6</li>
+        <li>item 7</li>
+        <li>item 8</li>
+      </ul>
+      </FadeIn>
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
@@ -17,7 +30,7 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
     </div>
   );
 }
